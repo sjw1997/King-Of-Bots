1512ab0b4c0fd2ac0664c41cc8c8a254a004659e
sjw1997 <sjww1997@126.com>
2023-09-06 08:28:36 +0800

    修改备案格式

--- web/src/components/ContentField.vue
+++ web/src/components/ContentField.vue
@@ -5,9 +5,10 @@
                 <slot></slot>
             </div>
         </div>
-        <div style="text-align: center; margin-top: 3vh;">
-            <a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2023022330号-1</a>
-        </div>
+    </div>
+
+    <div style="text-align: center; margin-top: 3vh;">
+        <a class="record" href="https://beian.miit.gov.cn/" target="_blank">京ICP备2023022330号-1</a>
     </div>
 </template>
 
@@ -18,5 +19,11 @@
 <style scoped>
 .content-field {
     margin-top: 20px;
+}
+
+.record {
+    color: gray;
+    text-decoration: none;
+    font-size: 15px;
 }
 </style>
